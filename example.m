ebeff2bcd5c7eee177b6411900b5633138880003
raudbergetemil <raemil@student.chalmers.se>
2018-12-12 19:30:06 +0100

    Added example function for reference

---
+++ example.m
@@ -0,0 +1,16 @@
+function example()
+%EXAMPLE - A reference function -
+%   ----------------------------------------------------------------------
+%   This is a reference on how the function should be documented.
+%   This is the standard for the built-in command 'help'
+%   Write a detailed explanation here so the function reference can be
+%   found easily.
+%   ----------------------------------------------------------------------
+%   First, write a brief explanation of the main purpose of the function.
+%   Then explain the input and output arguments
+%   ----------------------------------------------------------------------
+
+disp('-----------------------------');
+disp('This is an example function. Use "help example" to find more information')
+end
+
