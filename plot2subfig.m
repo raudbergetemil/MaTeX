7c5ebdf88125a6383f44735d15d719ac8ae1d6e1
Jens Rehn <jens@JENS-REHN-DEB.intrehnet>
2019-02-04 09:10:03 +0100

    Updated help text and error texts

--- plot2subfig.m
+++ plot2subfig.m
@@ -1,9 +1,10 @@
 function s = plot2subfig(figureHandler,names,name,folder, caption)
 % Input:
-% figureHandler takes the handle of the figure that should be converted to latex
-% folder 
-% name is the desired name for the picture and is also the label
-% caption is pretty clear
+% figureHandler is a list of figure handles of pictures to print
+% names is a list of names of each figure
+% name is the desired name for the group of / all pictures
+% folder is the folder to put the images inte, use "" if none
+% caption is a list of captions for each picture
 % 
 % Output:
 % s, the string which is copied into latex
@@ -11,6 +12,20 @@ function s = plot2subfig(figureHandler,names,name,folder, caption)
     if ~isfolder(folder)
         mkdir(folder)
     end
+    
+    n = length(figureHandler);
+    if ~ n == length(names) == length(caption)
+        error("figureHandler, names and caption needs to be of same length")
+    end
+    
+    if iscell(figureHandler) | iscell(names) | iscell(caption)
+        error("Use array and not cell arrays for figureHandler, names and caption")
+    end
+    
+    names = string(names);
+    name = string(name);
+    folder = string(folder);
+    caption = string(folder);
     
     figureHandler = figureHandler(:);
     s = "";
@@ -21,7 +36,7 @@ function s = plot2subfig(figureHandler,names,name,folder, caption)
         saveas(figureHandler(i), folder + "/" + names(i), "png");
     end
     
-    s = sprintf("%s\n\caption{}\n\\label{%s}\n\\end{figure}\n", [s,name]);
+    s = sprintf("%s\n\\caption{}\n\\label{%s}\n\\end{figure}\n", [s,name]);
     
     
 
