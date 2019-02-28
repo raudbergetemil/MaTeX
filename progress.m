c75893b7c21a39c79cdbfe2cbc2512f9880bf140
Jens Rehn <jens@JENS-REHN-DEB.intrehnet>
2019-02-28 09:18:54 +0100

    Improved function, added multiple argument choices therein the choice of GUI

--- progress.m
+++ progress.m
@@ -1,17 +1,45 @@
-function progress(p, width)
+function progress(p, varargin)
 %PROGRESS -
 %   ----------------------------------------------------------------------
-%   This function displays a progress bar provided a percentage.
-%   Do not print anything else while using this function.
+%   This function displays a progress bar provided a percentage. It checks
+%   wheter the user used -desktop or -nodesktop and uses that as basis for
+%   GUI or not. To override this, provide the function with -forceGUI or
+%   -forceCMD depending on wanted result. When using CMD provide the
+%   function with a width if not standard works for the current size of
+%   terminal.
+%
+%   Do not print anything else while using the CMD version of the function
 %   ----------------------------------------------------------------------
 %   INPUT:
-%   P in percent
+%   p
+%   OPTIONAL:
+%   forceGUI or forceCMD
+%   -width [value]  (Used for CMD to determine width of progressbar)
 %   ----------------------------------------------------------------------
+%   example use:
+%   progress(0.5)
+%   progress(0.1, 'forceCMD', -width, 80)
+%   progress(0.4, 'forceGUI')
 
-barLen  = round(width*p);
-space   = round(width*(1-p));
-bar = ['|', char(kron('=', ones(1, barLen))), '>', char(kron(' ', ones(1, space))), '|', char(string(p*100)), '%%\n'];
-clc;
-fprintf(bar)
+GUI = usejava('desktop');
+width = 70;
+for i = 1:nargin
+    if string(varargin{i}) == "forceGUI"
+        GUI = true;
+    elseif string(varargin{i}) == "forceCMD"
+        GUI = false;
+    elseif string(varargin{i}) == "-width"
+        width = cell2mat(varargin{i+1});
+    end
+end
 
+if GUI
+    waitbar(p, 'Progress')
+else
+    barLen  = round(width*p);
+    space   = round(width*(1-p));
+    bar = ['|', char(kron('=', ones(1, barLen))), '>', char(kron(' ', ones(1, space))), '|', char(string(p*100)), '%%\n'];
+    clc;
+    fprintf(bar)
+end
 end
