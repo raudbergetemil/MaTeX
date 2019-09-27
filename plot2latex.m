98e37ee27d642152b1d33f0b078be55d12175089
Jens <jens@Jens-deb.intrehnet>
2019-09-27 11:27:54 +0200

    Added possibility to output to file and improved error handlind

--- plot2latex.m
+++ plot2latex.m
@@ -1,15 +1,36 @@
-function s = plot2latex(figureHandler, name, folder, caption)
+function s = plot2latex(figureHandler, name, folder, caption, varargin)
 % Input:
 % figureHandler, takes the handle of the figure that should be converted to latex 
 % name, is the desired name for the picture and is also the label
 % folder, desired folder to save image in
 % caption, caption for the figure
+% Optional argumets are the fourth, an argument of 'true'
+% overwrites the file 'output' containing all matrices in latex form.
+% A value of false appends given matrix and if not any fourth argument
+% is provided the function will not write to any file.
 % 
+%
 % Output:
 % s, the string which is copied into latex
 
+    if nargin > 4
+        reinit = varargin{1};
+    end
+
     if ~isfolder(folder)
         mkdir(folder)
+    end
+
+    if ~ishandle(figureHandler)
+        error("First argument FIGUREHANDLER must be a handle to a figure")
+    elseif ~isstring(name)
+        error("Second argument NAME must be a string, \'%s\' is not a string", string(name));
+    elseif ~isstring(folder)
+        fprintf("Third argument FOLDER must be an string, \'%s\' is not a string", string(folder));
+    elseif ~isstring(caption)
+        fprintf("Forth argument caption must be an string, \'%s\' is not a string", string(caption));
+    elseif nargin > 4 && ~islogical(reinit)
+        error("Fourth argument REINIT, if provided, must be a logical");
     end
     
 
@@ -30,5 +51,12 @@ function s = plot2latex(figureHandler, name, folder, caption)
     
     s = comment_1 + s + comment_2;
 
+    if nargin > 4 && reinit
+        fprintf(fopen("output", 'w'), "%s", s);
+    elseif nargin > 4 && ~reinit
+        fprintf(fopen("output", 'a'), "%s", s);
+    else
+        return
+    end
         
 end
