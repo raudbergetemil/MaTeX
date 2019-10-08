6ef0131494655225c24b4a82302bfc5c3e1858da
Jens <jens@Jens-deb.intrehnet>
2019-10-08 09:13:23 +0200

    New function, comment() prints given string or character vector to output file

---
+++ comment.m
@@ -0,0 +1,22 @@
+function comment(str, reinit)
+    % SECTION  -
+    %   ----------------------------------------------------------------------
+    %   This function prints a comment in latex code to an output file
+    %   ----------------------------------------------------------------------
+    %   The function takes a string as first input and a boolean as second.
+    %   The boolean determines if the output should be appended to the output 
+    %   file or if it should overwrite its content.
+    %   ----------------------------------------------------------------------
+
+    if ~islogical(reinit)
+        error("The second argument must be a logical, ''%s'' is not a logical", reinit);
+    end
+    
+    str = string(str);
+    if reinit
+        fprintf(fopen("output", 'w'), "%s", str);
+    else
+        fprintf(fopen("output", 'a'), "%s", str);
+    end
+
+    end
